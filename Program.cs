bbfc094c85bb909e76d73a98b6395c6efa649d30
Samir L. Boulema <samirboulema@funda.nl>
2015-11-30 17:58:04 +0100

    Try disabling the missing keys warning

--- Program.cs
+++ Program.cs
@@ -19,6 +19,10 @@ namespace WebVRPano_Oculus
                 var tinyId = args[0].Split('/').Last();
                 var appDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
+                Environment.SetEnvironmentVariable("GOOGLE_API_KEY", "no", EnvironmentVariableTarget.User);
+                Environment.SetEnvironmentVariable("GOOGLE_DEFAULT_CLIENT_ID", "no", EnvironmentVariableTarget.User);
+                Environment.SetEnvironmentVariable("GOOGLE_DEFAULT_CLIENT_SECRET", "no", EnvironmentVariableTarget.User);
+
                 ProcessStartInfo startInfo = new ProcessStartInfo();
                 startInfo.FileName = Path.Combine(appDir, "chrome.exe");
                 startInfo.Arguments = $"--enable-webvr http://sboulema.nl:8080/{tinyId}";
