a835b747234bd5817c6af3b48950a31f9dedf458
Samir L. Boulema <samirboulema@funda.nl>
2015-12-01 12:22:32 +0100

    #3

--- Program.cs
+++ Program.cs
@@ -19,13 +19,9 @@ namespace WebVRPano_Oculus
                 var tinyId = args[0].Split('/').Last();
                 var appDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
-                Environment.SetEnvironmentVariable("GOOGLE_API_KEY", "no", EnvironmentVariableTarget.User);
-                Environment.SetEnvironmentVariable("GOOGLE_DEFAULT_CLIENT_ID", "no", EnvironmentVariableTarget.User);
-                Environment.SetEnvironmentVariable("GOOGLE_DEFAULT_CLIENT_SECRET", "no", EnvironmentVariableTarget.User);
-
                 ProcessStartInfo startInfo = new ProcessStartInfo();
                 startInfo.FileName = Path.Combine(appDir, "chrome.exe");
-                startInfo.Arguments = $"--enable-webvr http://sboulema.nl:8080/{tinyId}";
+                startInfo.Arguments = $"--enable-webvr --window-size=800,600 --app=http://sboulema.nl:8080/{tinyId}";
                 Process.Start(startInfo);
             }
         }
